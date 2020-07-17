e825329dec06932c26e5d95f0641ed092b220ea2
beata <beata@emc2wave.com>
2020-07-17 15:25:00 -0700

    create handleSubmitForm(e)

--- app.js
+++ app.js
@@ -2,3 +2,11 @@
 document.querySelector('form').addEventListener('submit', handleSubmitForm);
 
 // Event Handlers
+function handleSubmitForm(e) {
+  e.preventDefault();
+
+  let input = document.querySelector('input');
+  if (input.value !== '') addTodo(input);
+
+  input.value = '';
+}
